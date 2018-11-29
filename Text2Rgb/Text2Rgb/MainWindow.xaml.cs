78b783a75988d6129ce6cad86568c5e6ac34c6b7
Bish0pQ <tverswork@protonmail.com>
2018-11-29 23:08:08 +0100

    Added text loading Added worker

--- Text2Rgb/Text2Rgb/MainWindow.xaml.cs
+++ Text2Rgb/Text2Rgb/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Drawing;
 using Text2Rgb.Services;
 using System.IO;
 using Microsoft.Win32;
+using System.ComponentModel;
 
 namespace Text2Rgb
 {
@@ -27,9 +28,16 @@ namespace Text2Rgb
         ImageService imgService;
         Bitmap bm;
         Bitmap loadedBm;
+        string loadedText;
+        private BackgroundWorker bw;
         public MainWindow()
         {
             InitializeComponent();
+            bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.ProgressChanged += ProgressChanged;
+            bw.DoWork += DoWork;
+            bw.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
         }
 
 
@@ -37,21 +45,18 @@ namespace Text2Rgb
         {
             //Check if max-width is parseble
             int maxWidth;
+            string text = (!string.IsNullOrWhiteSpace(loadedText)) ? loadedText : txtInput.Text; // Check which text to use
 
             if (!int.TryParse(txtMaxWidth.Text, out maxWidth))
             {
                 MessageBox.Show("Please only type in whole numbers in the max width field.", "Error: you can only use whole numbers in the max-width field", MessageBoxButton.OK, MessageBoxImage.Error);
                 txtMaxWidth.Clear();
             }
-            else if(string.IsNullOrEmpty(txtInput.Text))
-            {
-                MessageBox.Show("The input in the inputfield can not be empty since there would be nothing to convert to RGB. Please typ in some text in the big textbox.", "Error: no input detected!");
-            }
             else
             {
                 try
                 {
-                    bm = imgService.CreateImage(txtInput.Text, maxWidth);
+                    bm = imgService.CreateImage(text, maxWidth);
                     btnSaveImage.IsEnabled = true;
                 }
                 catch (Exception ex)
@@ -59,7 +64,7 @@ namespace Text2Rgb
                     //Log exception
                     FileHelper.LogError(ex);
                 }
-                
+
             }
            
         }
@@ -76,13 +81,16 @@ namespace Text2Rgb
             /* Save the image locally */
             try
             {
-                string savePath = $"{Environment.CurrentDirectory}\\Images\\example.jpg";
+                string savePath = $"{Environment.CurrentDirectory}\\Images\\{DateTime.Now.ToShortDateString().Replace("/", "-")}.{DateTime.Now.ToShortTimeString().Replace(":","-")}.jpg";
                 if (File.Exists(savePath))
                 {
                     File.Delete(savePath);
                 }
 
                 bm.Save(savePath);
+
+                loadedText = string.Empty;
+                txtInput.Clear();
             }
             catch (Exception ex)
             {
@@ -130,6 +138,70 @@ namespace Text2Rgb
             {
                 MessageBox.Show("It seems you have not loaded an image yet! Please make sure you load an image prior to decoding!", "Error: no image has been loaded", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+
+            /* Loop through all pixels */
+            txtInput.Text = imgService.DecodeImage(loadedBm);
         }
+
+        private void BtnLoadText_Click(object sender, RoutedEventArgs e)
+        {
+            string path = string.Empty;
+
+            /* Show openfiledialog and set bitmap */
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //Settings
+            ofd.Filter = "Text Files (*.txt) | *.txt";
+            ofd.FilterIndex = 0;
+            ofd.Title = "Text2Rgb | Select a text file";
+
+            if (ofd.ShowDialog() == true)
+            {
+                path = ofd.FileName;
+            }
+
+            if (File.Exists(path))
+            {
+                try
+                {
+                    loadedText = File.ReadAllText(path);
+                    MessageBox.Show("Text has succesfully been loaded!", "Success: text has been loaded!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    FileHelper.LogError(ex);
+                    MessageBox.Show("Unable to open the text, please make sure the text file is not corrupted and try again.", "Error: could not load image!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+            }
+
+            if (loadedText.Length > txtInput.MaxLength)
+            {
+                txtInput.Text = "{TEXT IS LOADED IN MEMORY BUT TOO LARGE TO DISPLAY}";
+            }
+            else
+            {
+                txtInput.Text = loadedText;
+            }
+        }
+
+
+        #region Backgroundworker area
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Decode the picture
+        }
+
+        private void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Update progress
+            prgsReporter.Value = e.ProgressPercentage;
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Decoding is completed
+        }
+        #endregion
     }
 }
